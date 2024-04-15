6f3d9b4edced7ad7314b65e280a7513af68cf234
王世杰 <2660016236@qq.com>
2024-04-16 00:51:20 +0800

    初始化项目

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtLayout></NuxtLayout>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+            
+        </nav>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
